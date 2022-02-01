595332e06065170c734a95cc854b34d987e0e811
Thiago Arantes <thiagoarantes@gmail.com>
2022-01-31 20:16:33 -0500

    more Angular-like behavior in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
-
 export class AppComponent {
   title = "Tindermon";
+
+  isMenuOpen: boolean = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
